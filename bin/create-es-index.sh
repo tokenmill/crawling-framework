d4b277e0e074304d824cc8d4abe50b27fdb45807
Žygimantas Medelis <zzygis@gmail.com>
2017-09-18 14:19:35 +0300

    Configure ES indices via single paramterized script. Read ES index config from config file

---
+++ bin/create-es-index.sh
@@ -0,0 +1,10 @@
+# $1 - type of the index (docs, http_sources)
+# $2 - ES index config file name
+# $3 - ES host
+# $4 - application name
+export INDEX_URL="http://$3:9200/$4_$1_v1"
+
+
+curl -XDELETE "$INDEX_URL"
+curl -XPUT "$INDEX_URL" -d @elasticsearch/src/main/resources/indices/$2
+curl -XPUT "$INDEX_URL/_alias/$4_$1"
