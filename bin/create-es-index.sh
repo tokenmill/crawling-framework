8798c018430f7d4aca8968c4870558a2793b23e2
Žygimantas Medelis <zzygis@gmail.com>
2017-09-19 14:23:00 +0300

    Fix unerscore use in index naming

--- bin/create-es-index.sh
+++ bin/create-es-index.sh
@@ -2,9 +2,9 @@
 # $2 - ES index config file name
 # $3 - ES host
 # $4 - application name
-export INDEX_URL="http://$3:9200/$4_$1_v1"
+export INDEX_URL="http://$3:9200/$4-$1_v1"
 
 
 curl -XDELETE "$INDEX_URL"
 curl -XPUT "$INDEX_URL" -d @elasticsearch/src/main/resources/indices/$2
-curl -XPUT "$INDEX_URL/_alias/$4_$1"
+curl -XPUT "$INDEX_URL/_alias/$4-$1"
