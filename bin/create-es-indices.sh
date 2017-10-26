5f7a4147f9183e0da886c5b5dc410c1c38a61def
Dainius Jocas <dainius.jocas@tokenmill.lt>
2017-10-26 10:45:44 +0300

    typo

--- bin/create-es-indices.sh
+++ bin/create-es-indices.sh
@@ -3,7 +3,7 @@ if [ $# -eq 2 ] ; then
   bin/create-es-index.sh docs document.json $1 $2
   bin/create-es-index.sh named_queries query.json $1 $2
   bin/create-es-index.sh http_sources http_source.json $1 $2
-  bin/create-es-index.sh http_source_tests http_source_test http_source_test.json $1 $2
+  bin/create-es-index.sh http_source_tests http_source_test.json $1 $2
   bin/create-es-index.sh urls url.json $1 $2
 
 else
