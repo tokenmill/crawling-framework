66322b796c500e23eaa07767514c58763aaca962
Dainius Jocas <dainius.jocas@tokenmill.lt>
2017-12-19 23:22:28 +0200

    named query ops

--- elasticsearch/src/main/java/lt/tokenmill/crawling/es/EsNamedQueryOperations.java
+++ elasticsearch/src/main/java/lt/tokenmill/crawling/es/EsNamedQueryOperations.java
@@ -5,14 +5,22 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import lt.tokenmill.crawling.data.NamedQuery;
 import lt.tokenmill.crawling.data.PageableList;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchScrollRequest;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.elasticsearch.search.suggest.Suggest;
 import org.elasticsearch.search.suggest.SuggestBuilder;
 import org.elasticsearch.search.suggest.completion.CompletionSuggestion;
@@ -41,107 +49,119 @@ public class EsNamedQueryOperations extends BaseElasticOps {
     }
 
     public PageableList<NamedQuery> filter(String prefix) {
-        BoolQueryBuilder filter = QueryBuilders.boolQuery();
-        if (!Strings.isNullOrEmpty(prefix)) {
-            filter.must(QueryBuilders
-                    .prefixQuery("name", prefix.trim()));
+        try {
+            BoolQueryBuilder filter = QueryBuilders.boolQuery();
+            if (!Strings.isNullOrEmpty(prefix)) {
+                filter.must(QueryBuilders
+                        .prefixQuery("name", prefix.trim()));
+            }
+            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
+                    .fetchSource(true)
+                    .explain(false)
+                    .size(100)
+                    .query(filter);
+            SearchRequest searchRequest = new SearchRequest(getIndex())
+                    .types(getType())
+                    .source(searchSourceBuilder);
+            SearchResponse response = getConnection().getRestHighLevelClient()
+                    .search(searchRequest);
+            List<NamedQuery> items = Arrays.stream(response.getHits().getHits())
+                    .map(SearchHit::getSourceAsMap)
+                    .filter(Objects::nonNull)
+                    .map(this::mapToNamedQuery)
+                    .collect(Collectors.toList());
+            return PageableList.create(items, response.getHits().getTotalHits());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        SearchResponse response = getConnection().getClient().prepareSearch(getIndex())
-                .setTypes(getType())
-                .setPostFilter(filter)
-                .setSize(100)
-                .setFetchSource(true)
-                .setExplain(false)
-                .execute()
-                .actionGet();
-
-        List<NamedQuery> items = Arrays.stream(response.getHits().getHits())
-                .map(SearchHit::getSourceAsMap)
-                .filter(Objects::nonNull)
-                .map(this::mapToNamedQuery)
-                .collect(Collectors.toList());
-        return PageableList.create(items, response.getHits().getTotalHits());
+        return new PageableList<>();
     }
 
     public List<String> suggest(String prefix) {
-        CompletionSuggestionBuilder suggestionBuilder = new CompletionSuggestionBuilder("name_suggest")
-                .prefix(prefix);
-
-        SearchResponse response = getConnection().getClient().prepareSearch(getIndex())
-                .setTypes(getType())
-                .suggest(new SuggestBuilder().addSuggestion("suggestion", suggestionBuilder))
-                .setSize(100)
-                .setFetchSource(true)
-                .setExplain(false)
-                .execute()
-                .actionGet();
-
-        return response.getSuggest().filter(CompletionSuggestion.class).stream()
-                .flatMap(s -> s.getOptions().stream())
-                .sorted(Comparator.comparingDouble(Suggest.Suggestion.Entry.Option::getScore))
-                .map(Suggest.Suggestion.Entry.Option::getText)
-                .map(Text::toString)
-                .collect(Collectors.toList());
+        try {
+            CompletionSuggestionBuilder suggestionBuilder = new CompletionSuggestionBuilder("name_suggest")
+                    .prefix(prefix);
+            SearchSourceBuilder sourceRequestBuilder = new SearchSourceBuilder()
+                    .size(100)
+                    .explain(false)
+                    .fetchSource(true)
+                    .suggest(new SuggestBuilder().addSuggestion("suggestion", suggestionBuilder));
+            SearchRequest searchRequest = new SearchRequest(getIndex())
+                    .types(getType())
+                    .source(sourceRequestBuilder);
+            SearchResponse response = getConnection().getRestHighLevelClient()
+                    .search(searchRequest);
+            return response.getSuggest().filter(CompletionSuggestion.class).stream()
+                    .flatMap(s -> s.getOptions().stream())
+                    .sorted(Comparator.comparingDouble(Suggest.Suggestion.Entry.Option::getScore))
+                    .map(Suggest.Suggestion.Entry.Option::getText)
+                    .map(Text::toString)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
     }
 
 
     public NamedQuery get(String name) {
-        GetResponse response = getConnection().getClient()
-                .prepareGet(getIndex(), getType(), name.toLowerCase())
-                .setFetchSource(true)
-                .get();
-        if (response.isExists()) {
-            return mapToNamedQuery(response.getSource());
+        try {
+            GetResponse response = getConnection().getRestHighLevelClient()
+                    .get(new GetRequest(getIndex(), getType(), formatId(name))
+                            .fetchSourceContext(new FetchSourceContext(true)));
+            if (response.isExists()) {
+                return mapToNamedQuery(response.getSource());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
     public List<NamedQuery> all() {
-        Client client = getConnection().getClient();
-        TimeValue keepAlive = TimeValue.timeValueMinutes(10);
-        SearchResponse response = client.prepareSearch(getIndex())
-                .setTypes(getType())
-                .setQuery(QueryBuilders.matchAllQuery())
-                .setSize(100)
-                .setScroll(keepAlive)
-                .setFetchSource(true)
-                .setExplain(false)
-                .execute()
-                .actionGet();
-
-        List<NamedQuery> result = Lists.newArrayList();
-        do {
-            result.addAll(Arrays.stream(response.getHits().getHits())
-                    .map(SearchHit::getSourceAsMap)
-                    .filter(Objects::nonNull)
-                    .map(this::mapToNamedQuery)
-                    .collect(Collectors.toList()));
-            response = client.prepareSearchScroll(response.getScrollId())
-                    .setScroll(keepAlive)
-                    .execute()
-                    .actionGet();
-        } while (response.getHits().getHits().length != 0);
-        return result;
+        try {
+            TimeValue keepAlive = TimeValue.timeValueMinutes(10);
+            SearchRequest searchRequest = new SearchRequest(getIndex())
+                    .types(getType())
+                    .scroll(keepAlive)
+                    .source(new SearchSourceBuilder().query(QueryBuilders.matchAllQuery()));
+            SearchResponse response = getConnection().getRestHighLevelClient()
+                    .search(searchRequest);
+            List<NamedQuery> result = Lists.newArrayList();
+            do {
+                result.addAll(Arrays.stream(response.getHits().getHits())
+                        .map(SearchHit::getSourceAsMap)
+                        .filter(Objects::nonNull)
+                        .map(this::mapToNamedQuery)
+                        .collect(Collectors.toList()));
+                response = getConnection().getRestHighLevelClient()
+                        .searchScroll(new SearchScrollRequest(response.getScrollId())
+                                .scroll(keepAlive));
+            } while (response.getHits().getHits().length != 0);
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  Collections.emptyList();
     }
 
     public void save(NamedQuery nq) {
         try {
-            getConnection().getClient().prepareIndex(getIndex(), getType(), nq.getName().toLowerCase())
-                    .setSource(jsonBuilder()
-                            .startObject()
-                            .field("name", nq.getName())
-                            .startObject("name_suggest")
-                            .field("input", Lists.newArrayList(nq.getName()))
-                            .endObject()
-                            .field("stemmed_case_sensitive", nq.getStemmedCaseSensitive())
-                            .field("stemmed_case_insensitive", nq.getStemmedCaseInSensitive())
-                            .field("not_stemmed_case_sensitive", nq.getNotStemmedCaseSensitive())
-                            .field("not_stemmed_case_insensitive", nq.getNotStemmedCaseInSensitive())
-                            .field("advanced", nq.getAdvanced())
-                            .field("updated", new Date())
-                            .endObject())
-                    .get();
+            XContentBuilder contentBuilder = jsonBuilder()
+                    .startObject()
+                    .field("name", nq.getName())
+                    .startObject("name_suggest")
+                    .field("input", Lists.newArrayList(nq.getName()))
+                    .endObject()
+                    .field("stemmed_case_sensitive", nq.getStemmedCaseSensitive())
+                    .field("stemmed_case_insensitive", nq.getStemmedCaseInSensitive())
+                    .field("not_stemmed_case_sensitive", nq.getNotStemmedCaseSensitive())
+                    .field("not_stemmed_case_insensitive", nq.getNotStemmedCaseInSensitive())
+                    .field("advanced", nq.getAdvanced())
+                    .field("updated", new Date())
+                    .endObject();
+            IndexRequest indexRequest = new IndexRequest(getIndex()).source(contentBuilder);
+            getConnection().getRestHighLevelClient().index(indexRequest);
         } catch (IOException e) {
             LOG.error("Failed to save HTTP source test with url '{}'", nq.getName());
         }
@@ -150,34 +170,40 @@ public class EsNamedQueryOperations extends BaseElasticOps {
 
     public void delete(NamedQuery nq) {
         if (nq != null && nq.getName() != null) {
-            getConnection().getClient().prepareDelete(getIndex(), getType(), nq.getName()).get();
+            try {
+                getConnection().getRestHighLevelClient()
+                        .delete(new DeleteRequest(getIndex(), getType(), formatId(nq.getName())));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void deleteAll() {
-        Client client = getConnection().getClient();
+        try {
+            TimeValue keepAlive = TimeValue.timeValueMinutes(10);
+            SearchRequest searchRequest = new SearchRequest(getIndex())
+                    .scroll(keepAlive)
+                    .types(getType())
+                    .source(new SearchSourceBuilder()
+                            .size(100)
+                            .explain(false)
+                            .fetchSource(true)
+                            .query(QueryBuilders.matchAllQuery()));
 
-        TimeValue keepAlive = TimeValue.timeValueMinutes(10);
-        SearchResponse response = client.prepareSearch(getIndex())
-                .setTypes(getType())
-                .setPostFilter(QueryBuilders.matchAllQuery())
-                .setSize(100)
-                .setScroll(keepAlive)
-                .setFetchSource(true)
-                .setExplain(false)
-                .execute()
-                .actionGet();
-        do {
-           Arrays.stream(response.getHits().getHits())
-                    .map(SearchHit::getSourceAsMap)
-                    .filter(Objects::nonNull)
-                    .map(this::mapToNamedQuery)
-                    .forEach(this::delete);
-            response = client.prepareSearchScroll(response.getScrollId())
-                    .setScroll(keepAlive)
-                    .execute()
-                    .actionGet();
-        } while (response.getHits().getHits().length != 0);
+            SearchResponse response = getConnection().getRestHighLevelClient().search(searchRequest);
+            do {
+                Arrays.stream(response.getHits().getHits())
+                        .map(SearchHit::getSourceAsMap)
+                        .filter(Objects::nonNull)
+                        .map(this::mapToNamedQuery)
+                        .forEach(this::delete);
+                response = getConnection().getRestHighLevelClient()
+                        .searchScroll(new SearchScrollRequest(response.getScrollId()).scroll(keepAlive));
+            } while (response.getHits().getHits().length != 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private NamedQuery mapToNamedQuery(Map<String, Object> source) {
