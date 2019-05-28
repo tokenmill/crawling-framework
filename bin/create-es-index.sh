9e4ef71959862e355b4592f7fd50c01b157bfccc
Dainius Jocas <dainius.jocas@tokenmill.lt>
2019-05-28 15:50:18 +0300

    refactor: default index names

--- bin/create-es-index.sh
+++ bin/create-es-index.sh
@@ -2,9 +2,24 @@
 # $2 - ES index config file name
 # $3 - ES host
 # $4 - application name
-export INDEX_URL="http://$3:9200/$4-$1_v1"
+
+if [ -z "$4" ]
+then
+      export INDEX_URL="http://$3:9200/$1_v1"
+else
+      export INDEX_URL="http://$3:9200/$4-$1_v1"
+fi
 
 
 curl -H "Content-Type:application/json" -XDELETE "$INDEX_URL"
+echo
 curl -H "Content-Type:application/json" -XPUT "$INDEX_URL" -d @elasticsearch/src/main/resources/indices/$2
-curl -H "Content-Type:application/json" -XPUT "$INDEX_URL/_alias/$4-$1"
+echo
+if [ -z "$4" ]
+then
+      curl -H "Content-Type:application/json" -XPUT "$INDEX_URL/_alias/$1"
+      echo
+else
+      curl -H "Content-Type:application/json" -XPUT "$INDEX_URL/_alias/$4-$1"
+      echo
+fi
